c03b5f9431be7b6197e1aca1db39a193ad81a40d
Paul Lee <plee6950@gmail.com>
2020-09-30 16:01:20 +0100

    Fix cypress test

--- cypress/integration/booking-inquiry.js
+++ cypress/integration/booking-inquiry.js
@@ -36,6 +36,8 @@ describe("Booking inquiry form", () => {
         cy.get(".container > div > div > h1:first")
           .should("have.text", fixture.talent_full_name);
 
+        cy.wait(3000);
+
         cy.get("form").filter(':visible').within(() => {
           cy.get("input[name=talent_id]");
           cy.get("input[name='options.budget_currency']");
@@ -64,6 +66,8 @@ describe("Booking inquiry form", () => {
           failOnStatusCode: !devEnv
         }).should("include", fixture.talent_booking_url);
 
+        cy.wait(3000);
+
         cy.get("form").filter(':visible').within(() => {
           const date = new Date();
           date.setDate(date.getDate() + 3);
@@ -88,6 +92,8 @@ describe("Booking inquiry form", () => {
 
         // URL query format: ?user_id=user%40test.com&options%5Bevent_type_options%5D%5B0%5D=keynote&options%5Bnotes%5D=Bring%20your%20basketball%21&options%5Bevent_theme%5D=Fun%20NBA%20stories&options%5Bbudget_max_cents%5D=500000&options%5Bbudget_min_cents%5D=100000&options%5Bbudget_currency%5D=USD&options%5Bevent_date%5D=2020-06-04T22%3A00%3A00.000Z&options%5Bevent_type%5D=in_person&talent_id=88b89e96-97ec-4476-b8a7-0512cd0975c4
         cy.location("search").should("contain", `user_id=${encodeURIComponent(fixture.booking_inquiry_user_id)}`);
+
+        cy.wait(3000);
 
         cy.get(`img[src='${fixture.booking_inquiry_confirmation_visible_image}'`).should("be.visible", {
           timeout: 10000
