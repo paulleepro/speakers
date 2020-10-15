ef3e465a2269b1597862d0d238e8c79f9157722c
Paul Lee <plee6950@gmail.com>
2020-10-15 20:54:35 +0100

    Create Textarea component

---
+++ src/pages/TalentBookingNew/components/Textarea/styles.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import colors from "styles/colors";
+
+export const Input = styled.textarea`
+  object-fit: contain;
+  border-radius: 12px;
+  border: solid 1px ${colors.purpleLiner};
+  background-color: ${colors.black};
+
+  font-size: 24px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.5;
+  letter-spacing: 0.3px;
+  color: ${colors.white};
+
+  padding: 15px 36px 17px 24px;
+  outline: none;
+  margin-bottom: 16px;
+  width: 100%;
+
+  &::placeholder {
+    color: ${colors.midGrey};
+  }
+`;
