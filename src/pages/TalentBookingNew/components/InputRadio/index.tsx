60bd8e8f64aebbdc08ce7305cfe3c8e5a5479d25
Paul Lee <plee6950@gmail.com>
2020-10-16 15:51:41 +0100

    Make RadioInputOption as reusable

--- src/pages/TalentBookingNew/components/InputRadio/index.tsx
+++ src/pages/TalentBookingNew/components/InputRadio/index.tsx
@@ -16,31 +16,55 @@ interface IProps {
   name: string;
 }
 
+interface IOptionProps {
+  option: OptionType;
+  selected: number | string | undefined;
+  onChange: (value: number | string) => void;
+  name: string;
+}
+
+const RadioInputOption: FC<IOptionProps> = ({
+  option,
+  selected,
+  onChange,
+  name,
+}) => {
+  return (
+    <InputWrapper
+      active={option.value === selected}
+      onClick={() => onChange(option.value)}
+    >
+      <label>
+        <input
+          type="radio"
+          id={option.id}
+          name={name}
+          value={option.value}
+          checked={option.value === selected}
+        />
+        {option.label}
+        <RadioButton checked={option.value === selected} />
+      </label>
+    </InputWrapper>
+  );
+};
+
 const InputRadio: FC<IProps> = ({ options, onChange, selected, name }) => {
   return (
     <>
       {options.map((option: OptionType) => (
         <Col key={option.id} md={6}>
-          <InputWrapper
-            active={option.value === selected}
-            onClick={() => onChange(option.value)}
-          >
-            <label>
-              <input
-                type="radio"
-                id={option.id}
-                name={name}
-                value={option.value}
-                checked={option.value === selected}
-              />
-              {option.label}
-              <RadioButton checked={option.value === selected} />
-            </label>
-          </InputWrapper>
+          <RadioInputOption
+            option={option}
+            selected={selected}
+            onChange={onChange}
+            name={name}
+          />
         </Col>
       ))}
     </>
   );
 };
 
+export { RadioInputOption };
 export default InputRadio;
