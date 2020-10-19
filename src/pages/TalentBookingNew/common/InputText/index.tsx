625d99a5cc84a2f2e0f698699e9b3e00b616e377
Paul Lee <plee6950@gmail.com>
2020-10-19 17:00:38 +0100

    Add icon into InputText component

--- src/pages/TalentBookingNew/common/InputText/index.tsx
+++ src/pages/TalentBookingNew/common/InputText/index.tsx
@@ -1,22 +1,38 @@
 import React, { FC } from "react";
-
-import { Input } from "./styles";
+import { Input, IconWrapper, Wrapper } from "./styles";
 
 interface IProps {
   name: string;
   value?: number | string | undefined;
   placeholder?: string;
   onChange: (e: any) => void;
+  icon?: any;
+  margin?: string;
 }
 
-const InputText: FC<IProps> = ({ name, value, placeholder, onChange }) => {
+const InputText: FC<IProps> = ({
+  name,
+  value,
+  placeholder,
+  onChange,
+  icon,
+  margin,
+}) => {
   return (
-    <Input
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={onChange}
-    />
+    <Wrapper
+      width="100%"
+      flexDirection="row"
+      alignItems="center"
+      margin={margin}
+    >
+      <Input
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      {icon && <IconWrapper>{icon}</IconWrapper>}
+    </Wrapper>
   );
 };
 
--- src/pages/TalentBookingNew/common/InputText/styles.tsx
+++ src/pages/TalentBookingNew/common/InputText/styles.tsx
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 import colors from "styles/colors";
+import { Box } from "react-basic-blocks";
+
+export const Wrapper = styled(Box)<{ margin?: string }>`
+  position: relative;
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+`;
+
+export const IconWrapper = styled(Box)`
+  position: absolute;
+  right: 24px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+`;
 
 export const Input = styled.input`
   object-fit: contain;
@@ -15,9 +31,8 @@ export const Input = styled.input`
   letter-spacing: 0.3px;
   color: ${colors.white};
 
-  padding: 15px 36px 17px 24px;
+  padding: 15px 56px 17px 24px;
   outline: none;
-  margin-bottom: 16px;
   width: 100%;
 
   &::placeholder {
