066501d10ec92716a38a2660637752e539ec0ea7
Paul Lee <plee6950@gmail.com>
2020-10-20 14:43:26 +0100

    Create DatePicker component

---
+++ src/pages/TalentBookingNew/common/DatePicker/index.tsx
@@ -0,0 +1,42 @@
+import React, { FC, lazy } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+
+import { InputWrapper } from "./styles";
+import { Wrapper, IconWrapper, Label } from "../InputText/styles";
+
+const DatePicker = lazy(() => import("react-datepicker"));
+
+interface IProps {
+  icon?: any;
+  label?: string;
+  margin?: string;
+  onChange: (value: any) => void;
+  placeholder?: string;
+  value: Date;
+}
+
+const CustomDatePicker: FC<IProps> = ({
+  icon,
+  label,
+  margin,
+  onChange,
+  placeholder,
+  value,
+}) => {
+  return (
+    <Wrapper width="100%" flexDirection="row" alignItems="center" margin={margin}>
+      {label && <Label>{label}</Label>}
+      <InputWrapper>
+        <DatePicker
+          className="date-picker"
+          selected={value}
+          placeholderText={placeholder}
+          onChange={onChange}
+        />
+        {icon && <IconWrapper>{icon}</IconWrapper>}
+      </InputWrapper>
+    </Wrapper>
+  );
+};
+
+export default CustomDatePicker;
