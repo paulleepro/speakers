fee81f8491620e4831ab6c5ad2896ebee0c9b252
Paul Lee <plee6950@gmail.com>
2020-10-26 12:14:15 +0000

    Create form summary component

---
+++ src/pages/TalentBookingNew/components/FormSummary/styles.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import colors from "styles/colors";
+import { Box } from "react-basic-blocks";
+
+export const Wrapper = styled(Box)``;
+
+export const Title = styled.h4`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 1.5;
+  letter-spacing: 0.3px;
+  color: ${colors.white};
+  position: relative;
+  padding-right: 30px;
+
+  button {
+    position: absolute;
+    top: 8px;
+    right: 0;
+  }
+`;
+
+export const Field = styled.span`
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1.5;
+  letter-spacing: 0.2px;
+  color: ${colors.white};
+  margin-top: 4px;
+
+  span {
+    color: ${colors.midGrey};
+    margin-right: 3px;
+  }
+`;
+
+export const FieldWrapper = styled(Box)``;
+
+export const Border = styled.hr`
+  width: 100%;
+  height: 1px;
+  background-color: #2c2832;
+  border: none;
+  margin: 16px 0 32px 0;
+`;
