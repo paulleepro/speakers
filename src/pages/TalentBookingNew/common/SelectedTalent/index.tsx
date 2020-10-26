c6760a4164702dc210769fcac6a8ddb423cc2c04
Paul Lee <plee6950@gmail.com>
2020-10-26 13:27:54 +0000

    Create SelectedTalent component

---
+++ src/pages/TalentBookingNew/common/SelectedTalent/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from "react";
+import colors from "styles/colors";
+import { Button } from "styles/components";
+import CloseIcon from "assets/Icons/Close";
+import { Wrapper } from "./styles";
+
+interface IProps {
+  talent: any;
+  onRemove: (value: any) => void;
+  variant?: string;
+}
+
+const SelectedTalent: FC<IProps> = ({ talent, onRemove, variant }) => {
+  return (
+    <Wrapper variant={variant}>
+      {talent.name.raw}
+      <Button margin="0" padding="0" onClick={() => onRemove(talent)}>
+        <CloseIcon fill={colors.white} />
+      </Button>
+    </Wrapper>
+  );
+};
+
+export default SelectedTalent;
