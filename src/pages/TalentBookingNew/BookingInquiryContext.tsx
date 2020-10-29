b7a3547731cdb5962b35439e0f7ec8f24cb9b46c
Paul Lee <plee6950@gmail.com>
2020-10-29 18:25:58 -0400

    Revert "Update context for booking inquiry"
    This reverts commit aa368acef77b1b5df5d196e9a07c2f30877fc420.

--- src/pages/TalentBookingNew/BookingInquiryContext.tsx
+++ src/pages/TalentBookingNew/BookingInquiryContext.tsx
@@ -4,8 +4,6 @@ import { IBookingInquiry } from "./types";
 export interface IBookingInquiryContext {
   setBookingInquiry: (val: IBookingInquiry) => void;
   bookingInquiry: IBookingInquiry;
-  currentStep: number;
-  setCurrentStep: (value: number) => void;
 }
 
 export const BookingInquiryContext = createContext<IBookingInquiryContext>(
@@ -16,11 +14,10 @@ export const BookingInquiryProvider: FC = ({ children }) => {
   const [bookingInquiry, setBookingInquiry] = useState<IBookingInquiry>(
     {} as IBookingInquiry
   );
-  const [currentStep, setCurrentStep] = useState<number>(0);
 
   return (
     <BookingInquiryContext.Provider
-      value={{ bookingInquiry, setBookingInquiry, currentStep, setCurrentStep }}
+      value={{ bookingInquiry, setBookingInquiry }}
     >
       {children}
     </BookingInquiryContext.Provider>
